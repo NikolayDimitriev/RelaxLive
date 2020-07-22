552d991da1d17877b7c54c62e3b790678320b464
Nikolay from Orsk <dimitrievk56@mail.ru>
2020-07-22 16:53:29 +0500

    При клике на стрелочку открывается второй номер телефона

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,7 +1,33 @@
 //function
 
 const toggleMenu = () => {
-    console.log('hello');
+
+    //при клике на стрелоку открывается второй номер телефона
+    //создаю дополнительные классы 
+    const style = document.createElement('style');
+    style.textContent = `
+        .header-contacts__click {
+            position: relative;
+            opacity: 1 !important;
+        }
+        .header-contacts__arrow-transform {
+            transform: rotate(180deg);
+        }
+    `;
+    //добавляю их в документ
+    document.head.append(style);
+
+    //отлавливаю событие клика
+    document.querySelector('.header-contacts__arrow').addEventListener('click', () => {
+        document.querySelector('.header-contacts__phone-number-accord').classList.toggle('header-contacts__click');
+        document.querySelector('.header-contacts__phone-number-accord>.header-contacts__phone-number').classList.toggle('header-contacts__click');
+        document.querySelector('.header-contacts__arrow > img').classList.toggle('header-contacts__arrow-transform');
+    });
+
+
+
+
+;
 };
 
 
